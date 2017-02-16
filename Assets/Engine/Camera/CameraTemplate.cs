a3d2f6b5f355bad4f471cc13d63062a7947b275c
MalkavianDX <smkurvinen@gmail.com>
2017-02-16 16:56:00 +0200

    Adding Project Files
    Moving Unity project to Github

---
+++ Assets/Engine/Camera/CameraTemplate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract public class CameraTemplate : MonoBehaviour{
+
+    // variables that need to be
+	public float cameraSpeed = 15;
+	public float cameraZoomSpeed = 10;
+
+    //Abstract method to create a focuspoint for the camera
+    public abstract GameObject CreateFocusPoint();
+
+    //Abstract methods
+    public abstract void CameraKeyPressed ();
+
+    //Abstract methods that move the camera
+	public abstract Vector3 CameraMovement (int k);
+	public abstract Vector3 CameraRotation ();
+	public abstract Vector3 CameraOrbit ();
+	public abstract Vector3 CameraZoom (float scroll);
+	
+}
+
+
---
+++ Assets/Engine/Options/OptionsTemplate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class OptionsTemplate{
+
+    //For Future Use
+	public abstract void VideoOps();
+
+
+}
